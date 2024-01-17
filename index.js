53967bc4a2329ee75c710f67c9380181afc767d1
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-01-17 14:42:54 -0600

    finished workshop

---
+++ index.js
@@ -0,0 +1,21 @@
+//* To solve this exercise I will first create a string with our opening message and set an alert to notify the user. Then I will assign three variables to three different types of arithmetic whose answers are the combination of the lock. Last I will create another alert to notify the user of the combination lock *//
+
+const string = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+alert(string)
+
+//assigning three variables to three uniques arithmetic operator expressions//
+
+let comboOne = 5 * 2;
+let comboTwo = 25 + 15;
+let comboThree = 40 - 1;
+
+//console.log out each variable to make sure my arithmetic coincides with the combination lock.//
+
+console.log(comboOne)
+console.log(comboTwo)
+console.log(comboThree)
+
+// created a concatenated alert to notify the user //
+
+alert(comboOne + " " + comboTwo + " " + comboThree);
