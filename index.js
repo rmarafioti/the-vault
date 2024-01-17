7127498f7832f76157e88b69540e4ff3aa7fb939
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-01-17 15:23:24 -0600

    fixed typos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//* To solve this exercise I will first create a string with our opening message and set an alert to notify the user. Then I will assign three variables to three different types of arithmetic whose answers are the combination of the lock. Last I will create another alert to notify the user of the combination lock *//
+//* To solve this exercise I will first create a string with our opening message. Then I will assign three variables to three different types of arithmetic whose answers are the combination of the lock. Last I will create another alert to notify the user of the woth the message and the combination lock *//
 
 const string = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
 
@@ -8,17 +8,18 @@ let comboOne = 5 * 2;
 let comboTwo = 25 + 15;
 let comboThree = 40 - 1;
 
-
 //console.log out each variable to make sure my arithmetic coincides with the combination lock.//
 
 console.log(comboOne)
 console.log(comboTwo)
 console.log(comboThree)
 
-// created a concatenated alert to notify the user //
-
 const combo = " " + comboOne + "-" + comboTwo + "-" + comboThree
 
+//console log out to check if the concatenated combo expression works //
+
 console.log(combo)
+
+// created a concatenated alert to notify the user //
 
 alert(string + combo);
